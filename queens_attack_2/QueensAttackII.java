a48d8274585271e0149ded3c318d6a07bf252117
Ahmet Faruk Uzunkaya <sosyal.ahmet.fuzunkaya@gmail.com>
2021-05-12 16:19:47 +0300

    Update QueensAttackII.java

--- queens_attack_2/QueensAttackII.java
+++ queens_attack_2/QueensAttackII.java
@@ -7,9 +7,28 @@ class Solution
     public static void main(String[] args)
     {
 
+        int n = 5;
+        int k = 3;
+        int vezirYatay = 3;
+        int vezirDikey = 4;
+
         ArrayList<ArrayList<Integer>> engeller = new ArrayList<ArrayList<Integer>>();
 
-        System.out.println("Sonuc : " + queensAttack(5, 1, 3, 3, engeller));
+        int[][] engel_list = {
+            {5, 5},
+            {4, 2},
+            {2, 3}
+        };
+
+        for (int i = 0; i < engeller.size(); i++) {
+            for (int j = 0; j < engeller.get(i).size(); j++) {
+                //engeller.get(i).get(j). = engel_list[i][j];
+            }
+        }
+        
+
+
+        System.out.println("Sonuc : " + queensAttack(n, k, vezirYatay, vezirDikey, engeller));
         
         
     }
@@ -17,25 +36,25 @@ class Solution
 
     public static int queensAttack(int n, int k, int r_q, int c_q, ArrayList<ArrayList<Integer>> obstacles) 
     {
-    // Write your code here
-    int atakBolgeSayisi = 0;
-    
-    //engeller 1
-    //bosluklar 0
-    int[][] satrancTahta = new int[n][n];
-    
-    for(int i = 0; i < obstacles.size(); i++)
-    {
-        for(int j = 0; j < obstacles.get(i).size(); j++)
+        // Write your code here
+        int atakBolgeSayisi = 0;
+        
+        //engeller 1
+        //bosluklar 0
+        int[][] satrancTahta = new int[n][n];
+        
+        for(int i = 0; i < obstacles.size(); i++)
         {
-            satrancTahta[i][j] = obstacles.get(i).get(j);
+            for(int j = 0; j < obstacles.get(i).size(); j++)
+            {
+                satrancTahta[i][j] = obstacles.get(i).get(j);
+            }
         }
-    }
-    
-    
-    
-    
-    
-    return atakBolgeSayisi;
+        
+        
+        
+        
+        
+        return atakBolgeSayisi;
     }
 }
